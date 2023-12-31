965d3363215ddb793cee206536798f8dd851d169
rems-38 <rem.mazzone@orange.fr>
2023-12-31 14:22:16 +0100

    first nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,22 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import {Sora} from '@next/font/google'
+
+const sora = Sora({
+  subsets: ['latin-ext'],
+  variable: '--font-sora',
+  weights: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+
+const Layout = ({ children }) => {
+  return (
+    <div className={'page bg-site text-white bg-cover bg-no-repeat ${sira.variable} font-sora relative'}>
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
