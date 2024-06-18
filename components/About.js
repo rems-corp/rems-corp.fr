bb5c1e4d12e40106fe196872956a7ec666baab57
Maxime1708 <maximevidal260704@gmail.com>
2024-06-18 08:02:41 +0000

    ModifsAbout

--- components/About.js
+++ components/About.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import FramedText from '../components/FramedText';
-
 const About = () => {
     return (
         <section id="about" className="py-16 bg-gray-800 text-white h-screen">
             <div className="container mx-auto text-center">
                 <h2 className="text-4xl font-bold mb-4">About Us</h2>
                 <p className="text-lg">
-                Welcome to our amazing company. We are dedicated to transforming ideas into digital reality.
+                    Welcome to Rem's Corp, your partner in crafting beautiful and functional websites.
+                </p>
+                <p className="text-lg">
+                    Founded in [year] and located in Drôme, France, we specialize in developing websites for individuals and businesses.
+                </p>
+                <p className="text-lg">
+                    At Rem's Corp, we are dedicated to turning ideas into digital reality, delivering high-quality web solutions tailored to meet the unique needs of our clients.
                 </p>
             </div>
         </section>
     );
 }
 
-export default About
+export default About;
