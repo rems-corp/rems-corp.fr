b81e0c87a8d3c2d96af62402b68337b0261cf561
Maxime1708 <maximevidal260704@gmail.com>
2024-06-18 08:34:09 +0000

    ModernisationAboutUs

--- components/About.js
+++ components/About.js
@@ -2,16 +2,18 @@ const About = () => {
     return (
         <section id="about" className="py-16 bg-gray-800 text-white h-screen">
             <div className="container mx-auto text-center">
-                <h2 className="text-4xl font-bold mb-4 border-4 border-red-500 rounded-lg inline-block px-4 py-2">
-                    About Us
-                </h2>
-                <p className="text-lg">
+                <div className="bg-red-500 w-full py-4">
+                    <h2 className="text-4xl font-bold text-white">
+                        About Us
+                    </h2>
+                </div>
+                <p className="text-lg mt-4">
                     Welcome to Rem's Corp, your partner in crafting beautiful and functional websites.
                 </p>
-                <p className="text-lg">
+                <p className="text-lg mt-2">
                     Founded in [year] and located in Drôme, France, we specialize in developing websites for individuals and businesses.
                 </p>
-                <p className="text-lg">
+                <p className="text-lg mt-2">
                     At Rem's Corp, we are dedicated to turning ideas into digital reality, delivering high-quality web solutions tailored to meet the unique needs of our clients.
                 </p>
             </div>
