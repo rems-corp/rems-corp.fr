2e21a41dcd2348726876f64052c660497fb8f1d7
Maxime1708 <maximevidal260704@gmail.com>
2024-06-18 09:58:31 +0000

    formattage des textes about

--- components/About.js
+++ components/About.js
@@ -1,20 +1,35 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
 const About = () => {
     return (
-        <section id="about" className="py-16 bg-gray-800 text-white h-screen">
-            <div className="container mx-auto text-center">
+        <section
+            id="about"
+            className="relative py-16 bg-gray-800 text-white h-screen bg-cover bg-center bg-no-repeat"
+            
+        >
+            {/* Dégradé gris sur le fond */}
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-800 opacity-75"></div>
+
+            {/* Contenu principal */}
+            <div className="container mx-auto text-center relative z-10">
+                {/* Bande rouge pour "About Us" */}
                 <div className="bg-gradient-to-r from-red-500 to-red-700 w-full py-4 shadow-lg mb-8">
                     <h2 className="text-4xl font-bold text-white">
-                        About Us
+                        <FormattedMessage id="about.section.title" defaultMessage="About Us" />
                     </h2>
                 </div>
-                <p className="text-lg mt-4 transition-transform duration-1000 ease-in-out transform translate-y-0 opacity-100">
-                    Welcome to Rem's Corp, your partner in crafting beautiful and functional websites.
+
+                {/* Contenu textuel */}
+                <p className="text-lg mt-4 transition-opacity duration-1000 ease-in-out opacity-100">
+                    <FormattedMessage id="about.section.text1" />
                 </p>
-                <p className="text-lg mt-2 transition-transform duration-1000 ease-in-out transform translate-y-0 opacity-100 delay-200">
-                    Founded in [year] and located in Drôme, France, we specialize in developing websites for individuals and businesses.
+                <p className="text-lg mt-2 transition-opacity duration-1000 ease-in-out opacity-100 delay-200">
+                    <FormattedMessage id="about.section.text2" />
                 </p>
-                <p className="text-lg mt-2 transition-transform duration-1000 ease-in-out transform translate-y-0 opacity-100 delay-400">
-                    At Rem's Corp, we are dedicated to turning ideas into digital reality, delivering high-quality web solutions tailored to meet the unique needs of our clients.
+                <p className="text-lg mt-2 transition-opacity duration-1000 ease-in-out opacity-100 delay-400">
+                    <FormattedMessage id="about.section.text3" />
                 </p>
             </div>
         </section>
