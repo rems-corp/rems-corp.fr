4e8a5a4b50bb4cdf9f01023a2efc28a34bfebe71
Maxime1708 <maximevidal260704@gmail.com>
2024-06-18 08:32:54 +0000

    ModifsAboutUs

--- components/About.js
+++ components/About.js
@@ -2,7 +2,9 @@ const About = () => {
     return (
         <section id="about" className="py-16 bg-gray-800 text-white h-screen">
             <div className="container mx-auto text-center">
-                <h2 className="text-4xl font-bold mb-4">About Us</h2>
+                <h2 className="text-4xl font-bold mb-4 border-4 border-red-500 rounded-lg inline-block px-4 py-2">
+                    About Us
+                </h2>
                 <p className="text-lg">
                     Welcome to Rem's Corp, your partner in crafting beautiful and functional websites.
                 </p>
