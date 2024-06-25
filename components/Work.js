a936fefa799235cc9c8f2e951589139ccf6b711f
Maxime1708 <maximevidal260704@gmail.com>
2024-06-25 08:04:13 +0000

    Ajout image de fond + images exemple pour chaque projet

--- components/Work.js
+++ components/Work.js
@@ -2,33 +2,37 @@ import React from 'react';
 
 const projects = [
     {
-        title: 'image Premier projet',
-        description: 'Description for project one. Texte explicatif.',
-        image: 'project1.jpg',
+        title: 'Premier projet',
+        description: 'Description for project one. Texte explicatif',
+        image: 'project1.png',
     },
     {
-        title: 'image Deuxième projet',
-        description: 'Description for project two. Texte explicatif.',
-        image: 'project2.jpg',
+        title: 'Second projet',
+        description: 'Description for project two. Texte explicatif',
+        image: 'project2.png',
     },
     {
-        title: 'image Troisième projet',
-        description: 'Description for project three. Texte explicatif.',
-        image: 'project3.jpg',
+        title: 'Troisième projet',
+        description: 'Description for project three. Texte explicatif',
+        image: 'project3.png',
     },
 ];
 
 const Work = () => {
     return (
-        <section id="work" className="py-16 bg-gray-800 text-white h-full">
+        <section 
+            id="work" 
+            className="py-16 bg-gray-800 text-white h-full bg-cover bg-center" 
+            style={{ backgroundImage: 'url(workbg.png)' }}
+        >
             <div className="container mx-auto text-center">
                 <h2 className="text-4xl font-bold mb-8">Our Work</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {projects.map((project, index) => (
-                        <div key={index} className="bg-gray-700 rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
+                        <div key={index} className="bg-gray-700 bg-opacity-75 rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
                             <img src={project.image} alt={project.title} className="w-full h-48 object-cover" />
                             <div className="p-6">
-                                <h3 className="text-2xl font-bold mb-2">{project.title}</h3> {/* Titre en gras*/}
+                                <h3 className="text-2xl font-bold mb-2">{project.title}</h3>
                                 <p className="text-gray-300">{project.description}</p>
                             </div>
                         </div>
