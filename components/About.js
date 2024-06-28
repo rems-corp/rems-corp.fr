081f66ea98d292b3e8bc3a6e0878ef39b1d96ca7
Maxime1708 <maximevidal260704@gmail.com>
2024-06-28 10:34:46 +0000

    Testimonials + About modifs + lang fr & en

--- components/About.js
+++ components/About.js
@@ -6,12 +6,10 @@ const About = () => {
         <section id="about" className="min-h-screen bg-cover bg-center flex items-center" style={{ backgroundImage: `url('/logopc.png')` }}>
             <div className="container mx-auto px-4 py-24 text-center text-white">
                 <div className="max-w-3xl mx-auto">
-                    <div className="bg-red-600 w-full py-4 shadow-lg mb-8 rounded-xl">
-                        <h2 className="text-4xl md:text-5xl font-bold text-white">
-                            <FormattedMessage id="about.section.title" defaultMessage="À propos de nous" />
-                        </h2>
-                    </div>
-                    <p className="text-lg md:text-xl leading-relaxed mb-8">
+                    <h2 className="text-4xl md:text-5xl font-bold text-white mb-8">
+                        <FormattedMessage id="about.section.title" defaultMessage="About Us" />
+                    </h2>
+                    <p className="text-lg md:text-xl leading-relaxed mb-8 text-white">
                         <FormattedMessage
                             id="about.section.description"
                             defaultMessage="Welcome to Rem's Corp, your partner in crafting beautiful and functional websites. Founded in 2023 and located in Drôme, France, we specialize in developing websites for individuals and businesses. At Rem's Corp, we are dedicated to turning ideas into digital reality, delivering high-quality web solutions tailored to meet the unique needs of our clients."
@@ -21,10 +19,10 @@ const About = () => {
 
                 <div className="flex justify-center space-x-4">
                     <div className="max-w-xs p-4 bg-gray-800 bg-opacity-50 rounded-lg">
-                        <h3 className="text-xl font-bold mb-2">
+                        <h3 className="text-xl font-bold mb-2 text-white">
                             <FormattedMessage id="about.section.service1.title" defaultMessage="Web Design" />
                         </h3>
-                        <p className="text-sm md:text-base">
+                        <p className="text-sm md:text-base text-white">
                             <FormattedMessage
                                 id="about.section.service1.description"
                                 defaultMessage="We create stunning and user-friendly designs that captivate your audience."
@@ -32,10 +30,10 @@ const About = () => {
                         </p>
                     </div>
                     <div className="max-w-xs p-4 bg-gray-800 bg-opacity-50 rounded-lg">
-                        <h3 className="text-xl font-bold mb-2">
+                        <h3 className="text-xl font-bold mb-2 text-white">
                             <FormattedMessage id="about.section.service2.title" defaultMessage="Web Development" />
                         </h3>
-                        <p className="text-sm md:text-base">
+                        <p className="text-sm md:text-base text-white">
                             <FormattedMessage
                                 id="about.section.service2.description"
                                 defaultMessage="We create robust and scalable websites tailored to your business needs."
@@ -43,10 +41,10 @@ const About = () => {
                         </p>
                     </div>
                     <div className="max-w-xs p-4 bg-gray-800 bg-opacity-50 rounded-lg">
-                        <h3 className="text-xl font-bold mb-2">
+                        <h3 className="text-xl font-bold mb-2 text-white">
                             <FormattedMessage id="about.section.service3.title" defaultMessage="Optimization" />
                         </h3>
-                        <p className="text-sm md:text-base">
+                        <p className="text-sm md:text-base text-white">
                             <FormattedMessage
                                 id="about.section.service3.description"
                                 defaultMessage="We optimize your website to rank higher on search engines."
