852f742f128c604d554af0c1dbd57912cf39663d
Rem's 38 <57954491+rems-38@users.noreply.github.com>
2024-07-02 11:41:34 +0000

    fix z-index of lang btn (nav in bottom)

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const Header = ({ locale, setLocale }) => {
     <header className="absolute z-30 w-full">
       <h1 className="h1 text-3xl xl:pl-12 xl:pt-6 pt-3 pl-4">REM'S CORP<span className="text-accent">.</span></h1>
 
-      <button onClick={toggleLocale} className='absolute top-6 right-6 bg-primary p-2 rounded-lg text-white'>
+      <button onClick={toggleLocale} className='absolute top-6 right-6 bg-primary p-2 rounded-lg text-white z-0'>
         {locale === "fr" ? "Switch to English" : "Passer en Français"}
       </button>
       
